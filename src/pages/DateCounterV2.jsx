4ff0f4bc868cf2ae2753700bea5847756b9167f9
RezaPahlevi1 <flashwatchreza@gmail.com>
2025-10-20 13:24:10 +0700

    feat: change usestate in datecounterv2 to usereducer

--- src/pages/DateCounterV2.jsx
+++ src/pages/DateCounterV2.jsx
@@ -1,11 +1,52 @@
-import React from "react";
-import { useState } from "react";
+import React, { useReducer } from "react";
+
+function Button({ onClick, className = "", variant = "default", children }) {
+  let baseStyle =
+    "px-4 py-2 rounded-lg font-semibold text-white transition duration-200 disabled:opacity-50 disabled:cursor-not-allowed";
+
+  let variantStyle = "";
+  switch (variant) {
+    case "increment":
+      variantStyle = "bg-green-600 hover:bg-green-700";
+      break;
+
+    case "decrement":
+      variantStyle = "bg-red-600 hover:bg-red-700";
+      break;
+    default:
+      variantStyle = "bg-gray-600 hover:bg-gray-700";
+  }
+  return (
+    <button
+      onClick={onClick}
+      className={`${baseStyle} ${variantStyle} ${className}`}
+    >
+      {children}
+    </button>
+  );
+}
+
+function reducer(state, action) {
+  if (action.type === "inc") return state + 1;
+  if (action.type === "dec") return state - 1;
+  if (action.type === "reset") return 0;
+  if (action.type === "setCount") return action.payload;
+  return state;
+}
 
 function DateCounterV2() {
-  const [count, setCount] = useState(null);
+  const [count, dispatch] = useReducer(reducer, 0);
+
+  function handleInc() {
+    dispatch({ type: "inc" });
+  }
+
+  function handleDec() {
+    dispatch({ type: "dec" });
+  }
 
   function handleReset() {
-    setCount(0);
+    dispatch({ type: "reset" });
   }
 
   const date = new Date();
@@ -14,33 +55,52 @@ function DateCounterV2() {
   return (
     <div className="w-full flex flex-col items-center justify-start min-h-screen py-10 ">
       <h1 className="text-3xl font-bold text-blue-600 mb-6">Date Counter V2</h1>
-      <form className="mb-5">
+      <form className="mb-5" onSubmit={(e) => e.preventDefault()}>
         <input
           type="range"
           min="0"
           max="100"
           value={count}
-          onChange={(e) => setCount(Number(e.target.value))}
+          onChange={(e) =>
+            dispatch({ type: "setCount", payload: Number(e.target.value) })
+          }
           className="w-full text-blue-600 accent-blue-600"
         />{" "}
-        <input
-          type="number"
-          value={count}
-          onChange={(e) => setCount(e.target.value)}
-          className="border border-gray-300 p-2 rounded-lg"
-        />
+        <div className="flex flex-row gap-2">
+          <Button onClick={handleDec} variant="decrement">
+            -
+          </Button>
+          <input
+            type="number"
+            value={count === 0 ? "" : count}
+            onChange={(e) => {
+              const val = e.target.value;
+              dispatch({
+                type: "setCount",
+                payload: val === "" ? 0 : Number(val),
+              });
+            }}
+            className="border border-gray-300 p-2 rounded-lg"
+          />
+          <Button onClick={handleInc} variant="increment">
+            +
+          </Button>
+        </div>
       </form>
       <span>
         Hari ini {count} hari dari {date.toDateString()}
       </span>
 
       {count !== 0 && (
-        <button
-          className="mt-4 bg-red-500 text-white py-2 px-4 rounded-lg cursor-pointer"
-          onClick={handleReset}
-        >
+        // <button
+        //   className="mt-4 bg-red-500 text-white py-2 px-4 rounded-lg cursor-pointer"
+        //   onClick={handleReset}
+        // >
+        //   Reset
+        // </button>
+        <Button onClick={handleReset} variant="decrement">
           Reset
-        </button>
+        </Button>
       )}
     </div>
   );
